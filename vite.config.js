a1d44ebbdb2f487e3df9f5f0b62578b1965036ed
Souravkarmakar_Mayabious <souravk.mayabious@gmail.com>
2025-04-30 18:51:05 +0530

    change vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // This lets other devices on your network access the dev server
+    port: 5173        // Optional: customize the port
+  }
 })
